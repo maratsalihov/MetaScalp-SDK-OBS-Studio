678a0df38c2dc2c7ccc01f293cc28c9cd6d1975b
marat <maratsalihov@gmail.com>
2026-04-23 14:54:44 +0500

    Initial commit

---
+++ create_repo.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\GitHub CLI\gh.exe" repo create "MetaScalp SDK + OBS Studio" --public
---
+++ run_create_repo.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+"C:\Program Files\GitHub CLI\gh.exe" repo create "MetaScalp SDK + OBS Studio" --public
+pause
